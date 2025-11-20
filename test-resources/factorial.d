bdb467487626baef735e0d27f65f159f4b38595b
Timofey Ivlev <mastermigkc@gmail.com>
2025-11-20 21:24:51 +0300

    update report for Interpreter

--- test-resources/factorial.d
+++ test-resources/factorial.d
@@ -1,12 +1,2 @@
-var factorial := func(n) is
-    if n <= 1 then
-        return 1
-    else
-        return n * factorial(n - 1)
-    end
-end
-
-for i in 1..5 loop
-    print factorial(i)
-    if i = 3 => exit
-end
+var n := 1 + 1
+print n
